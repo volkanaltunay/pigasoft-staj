3d4f36859d1e2aa659e91c097f1a441a212d90e1
volkanaltunay <volkan-altunay@outlook.com>
2024-07-12 11:57:25 +0300

    owl carousel

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,19 @@
-
+$('.owl-carousel').owlCarousel({
+    center:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplayHoverPause:true,
+    smartSpedd:1500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
